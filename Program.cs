37ee7e67801773af67ff4a5c918fba19807d4c4e
Gary Smith <unity21047@gmail.com>
2021-04-01 22:18:17 -0500

    Major Update:
    Added the project fundation.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _2021_04_01_Mongame_ImGui_Study
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
